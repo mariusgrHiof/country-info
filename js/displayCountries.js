7d39ebc40186be73523b47868c00bc9c0e0ebde5
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2021-08-22 12:16:55 +0200

    layout

--- js/displayCountries.js
+++ js/displayCountries.js
@@ -1,9 +1,9 @@
-import fetchAllCountries from "./fetchAllCountries.js";
-import formatNumber from "./utils/formatNumberWithCommas.js";
-import setCountry from "./setCountry.js";
+import fetchAllCountries from './fetchAllCountries.js';
+import formatNumber from './utils/formatNumberWithCommas.js';
+import setCountry from './setCountry.js';
 
 const displayCountries = async (url) => {
-  const countryListEl = document.querySelector(".country-list");
+  const countryListEl = document.querySelector('.country-list');
 
   const countries = await fetchAllCountries(url);
 
@@ -19,31 +19,37 @@ const displayCountries = async (url) => {
   countryListEl.innerHTML = countries
     .map(({ flag, name, population, region, capital, alpha3Code }) => {
       return `
-     <a href="./countryDetail.html"> 
-      <article class="country" data-id=${alpha3Code}>
-          <div class="img-container">
-            <img src=${flag} class="country__flag" alt="">
-          </div>
-          <div class="country__infos">
-            <h2 class="country__name">${name}</h2>
-            <div class="country__info">
-              <label for="country__population">Population:</label>
-              <p class="country__population">${formatNumber(population)}</p>
-            </div>
-            <div class="country__info">
-             <label for="country__region">Region:</label>
-              <p class="country__region">${region}</p>
-            </div>
-            <div class="country__info">
-              <label for="country__capital">Capital:</label>
-              <p class="country__capital">${capital}</p>
-            </div>
-          </div>  
-      </article>
-    </a>
+      
+       
+        <a href="./countryDetail.html"> 
+          <article class="country" data-id=${alpha3Code}>
+              <div class="img-container">
+                <img src=${flag} class="country__flag" alt="">
+              </div>
+              <div class="country__infos">
+                <h3 class="country__name">${name}</h3>
+                <div class="country__info">
+                  <label for="country__population">Population:</label>
+                  <p class="country__population">${formatNumber(population)}</p>
+                </div>
+                <div class="country__info">
+                <label for="country__region">Region:</label>
+                  <p class="country__region">${region}</p>
+                </div>
+                <div class="country__info">
+                  <label for="country__capital">Capital:</label>
+                  <p class="country__capital">${capital}</p>
+                </div>
+              </div>  
+          </article>
+        </a>
+     
+      
+     
+     
     `;
     })
-    .join("");
+    .join('');
 };
 
 export default displayCountries;
